cb38e2f8ab54db29c147f2ed9c0650d95f2528bd
JBobda <J.BobdaT@gmail.com>
2019-01-10 15:22:27 -0600

    Completed the LoginForm for the most part along with authentication from firebase, created the base for the spinner

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,18 +1,59 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-import { Card, CardSection, Button } from './common';
+import { Text, View } from 'react-native';
+import firebase from 'firebase';
+import { Card, CardSection, Button, Input } from './common';
 
 class LoginForm extends Component {
+    state = {
+        email: '',
+        password: '',
+        error: ''
+    };
+
+    onButtonPress() {
+        const { email, password } = this.state;
+
+        this.setState({ error: '' });
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .catch(() => {
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+            .catch(() => {
+                this.setState({error: 'Authentication Failed'});
+            });
+        });
+    }
+
     render() {
         return (
             <Card>
                 <CardSection>
-                    <TextInput style={{height: 20, width: 100}}></TextInput>
+                    <Input 
+                        label={'Email'}
+                        placeholder={'user@gmail.com'}
+                        value={this.state.email}
+                        onChangeText={text => this.setState({email: text})}
+                    />
                 </CardSection>
+
                 <CardSection>
+                    <Input
+                        label={'Password'}
+                        secureTextEntry
+                        placeholder={'password'}
+                        value={this.state.password}
+                        onChangeText={text => this.setState({password: text})}
+                    />
                 </CardSection>
+                
+                <View style={styles.viewStyle}>
+                    <Text style={styles.errorTextStyle}>
+                        {this.state.error}
+                    </Text>
+                </View>
+
                 <CardSection>
-                    <Button>
+                    <Button onPress={this.onButtonPress.bind(this)}>
                         Log in
                     </Button>
                 </CardSection>
@@ -20,5 +61,16 @@ class LoginForm extends Component {
         );
     }
 }
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
+    },
+    viewStyle: {
+        backgroundColor: '#fff'
+    }
+};
 
 export default LoginForm;
